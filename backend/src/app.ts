f59642d51e0eb476c49b5f3dba10eb5abd8e337d
cegredev <cedricg@gmx.net>
2022-08-25 16:53:47 +0200

    sfsfsdf

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,34 +1,30 @@
-// import express from "express";
-// // import routes from "./routes.js";
-// // import helmet from "helmet";
-// // // import connection from "./database.js";
-// // import cors from "cors";
-// // import logger from "./logger.js";
+import express from "express";
+import routes from "./routes.js";
+import helmet from "helmet";
+import connection from "./database.js";
+import cors from "cors";
+import logger from "./logger.js";
 
-// const app = express();
-// // app.use(helmet());
-// // app.use(express.json());
-// // app.use(cors({ origin: ["http://localhost:3000", "https://zeitungsfahrer-test.cedricgreiten.com"] }));
-// // app.use((req, res, next) => {
-// // 	logger.info(req.method + " " + req.originalUrl + " " + JSON.stringify(req.body));
-// // 	next();
-// // });
+const app = express();
+app.use(helmet());
+app.use(express.json());
+app.use(cors({ origin: ["http://localhost:3000", "https://zeitungsfahrer-test.cedricgreiten.com"] }));
+app.use((req, res, next) => {
+	logger.info(req.method + " " + req.originalUrl + " " + JSON.stringify(req.body));
+	next();
+});
 
-// const PORT = process.env.port || 3001;
+const PORT = process.env.port || 3001;
 
-// console.log("port", PORT);
+routes(app);
 
-// // routes(app);
+const server = app.listen(PORT, () => {
+	logger.info(`Application listening at port ${PORT}`);
+});
 
-// const server = app.listen(PORT, () => {
-// 	// logger.info(`Application listening at port ${PORT}`);
-// });
+process.on("exit", () => {
+	logger.info("Shutting down!");
 
-console.log("Hi!");
-
-// process.on("exit", () => {
-// 	logger.info("Shutting down!");
-
-// 	// connection.end();
-// 	server.close();
-// });
+	connection.end();
+	server.close();
+});
