0da5912f3186e836cf227b9d7d2303fe6dee86a9
cegredev <57912512+cegredev@users.noreply.github.com>
2022-11-21 14:53:59 +0100

    tried more stuff

--- backend/nope.puppeteerrc.cjs
+++ backend/nope.puppeteerrc.cjs
@@ -1,9 +1,13 @@
 const { join } = require("path");
+import env from "src/env.js";
+
+console.log("own env:", env);
+console.log("process.env", process.env);
 
 /**
  * @type {import("puppeteer").Configuration}
  */
 module.exports = {
 	// Changes the cache location for Puppeteer.
-	cacheDirectory: process.env.CHANGE_PUPPETEER_DIR ? join(__dirname, ".cache", "puppeteer") : undefined,
+	cacheDirectory: env.CHANGE_PUPPETEER_DIR ? join(__dirname, ".cache", "puppeteer") : undefined,
 };
