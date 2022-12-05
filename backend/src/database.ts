d4fc98ab246017267136b490e53808043f302431
cegredev <57912512+cegredev@users.noreply.github.com>
2022-12-05 11:30:03 +0100

    updated env variable mechanism

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -2,16 +2,19 @@ import mysql from "mysql2/promise";
 import env from "./env.js";
 
 const pool = mysql.createPool({
-	user: env.DB_USER,
-	database: env.DATABASE,
-	password: env.DB_PASSWORD,
-	port: parseInt(env.DB_PORT!),
-	host: env.DB_HOSTNAME,
+	user: env.DB_USER || process.env.DB_USER,
+	database: env.DATABASE || process.env.DATABASE,
+	password: env.DB_PASSWORD || process.env.DB_PASSWORD,
+	port: parseInt(env.DB_PORT || process.env.DB_PORT),
+	host: env.DB_HOSTNAME || process.env.DB_HOSTNAME,
 	timezone: "+00:00",
 });
 
+console.log(process.env);
+console.log(env);
+
 // Was needed on one of my testing machines idk
-pool.execute("SET GLOBAL sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));");
+// pool.execute("SET GLOBAL sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));");
 
 export interface RouteReport {
 	code: number;
